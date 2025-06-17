23af1fc600bd3320db061da12f18840a9f9b0912
karthik <karthikaddanki11@gmail.com>
2025-06-17 22:28:28 +0530

    added some code changes into the accounts

--- src/main/java/com/eaxybank/accounts/AccountsApplication.java
+++ src/main/java/com/eaxybank/accounts/AccountsApplication.java
@@ -1,7 +1,34 @@
 package com.eaxybank.accounts;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImp")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Accounts Microservices REST API Documentation",
+                description = "EazyBank Accounts microservices REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name="Karthik Addanki",
+                        email = "accounts@gmail.com",
+                        url="https://localhost:8080"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url ="https://www.eazybytes.com"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Eazybank Accounts Microservices RESTAPI Documentation",
+                url="https://www.eazybytes.com/swager-ui.html"
+        )
+)
 public class AccountsApplication {
     public static void main(String[] args) {
         SpringApplication.run(AccountsApplication.class, args);
--- src/main/java/com/eaxybank/accounts/dto/CustomerDto.java
+++ src/main/java/com/eaxybank/accounts/dto/CustomerDto.java
@@ -1,10 +1,20 @@
 package com.eaxybank.accounts.dto;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 @Data
 public class CustomerDto {
 
+    @NotEmpty(message="NAME SHOULD NOT BE A NULL")
+    @Size(min=3,max=30,message="Name should be in the length of 3 to 30")
     private String name;
+    @NotEmpty(message="email should not be an empty")
+    @Email(message="email should be valid")
     private String email;
+    @Pattern(regexp = "(^$|[0-9]{10})",message = "Length of the mobile number is 10 ")
     private String mobileNumber;
     private AccountsDto accountsDto;
 }
